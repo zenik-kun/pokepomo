5ed06270776b1b30c82d97ca7d1b99742cea2bd7
zenik-kun <nikirukyun@gmail.com>
2024-07-04 08:20:08 +0530

    TimerHeader.jsx done.

---
+++ components/TimerHeader.jsx
@@ -0,0 +1,22 @@
+import { Text } from 'react-native'
+import React from 'react'
+
+const TimerHeader = ({ intervalType, running }) => {
+    const handleDisplay = () => {
+        if (intervalType === "Working") {
+            return running? "Gotta Catch 'em All!" : "Ikuzo!";
+        } else {
+            return running? "It's break time! Enjoy" : "Relax :)" ;
+        };
+    };
+
+    const textToShow = handleDisplay();
+
+    return (
+        <Text className = "text-3xl text-center text-white font-pokemon tracking-wider p-5">
+            {textToShow}
+        </Text>
+    )
+}
+
+export default TimerHeader
